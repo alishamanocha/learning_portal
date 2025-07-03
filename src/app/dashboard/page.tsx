9959f355d4e3aada5a8a63e8244207e0d5a155d3
Alisha Manocha <alisha.manocha04@gmail.com>
2025-07-02 20:19:55 -0700

    created a dashboard component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,9 @@
 "use client";
+import Dashboard from "../components/Dashboard/Dashboard";
 import withAuth from "../components/withAuth";
 
 function DashboardPage() {
-    return <h1>This is the dashboard</h1>
+    return <Dashboard />
   }
 
 export default withAuth(DashboardPage);
